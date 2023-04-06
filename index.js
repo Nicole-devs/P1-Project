ea9ae916bc76839b50d5f9247c86032844e2109d
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-04-06 15:08:02 +0300

    Done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded",() =>{
     .then (response => response.json())
     .then (data => {
         let container = document.createElement('div')
+        container.setAttribute('id','teams')
         document.querySelector("body").appendChild(container)
         data.teams.forEach(element => {
             const name=document.createElement("h2")
@@ -30,13 +31,11 @@ document.addEventListener("DOMContentLoaded",() =>{
                 pitch.textContent = element.strStadium
                 div.appendChild(pitch)
                 document.querySelector("body").appendChild(pitch)
-                
-            
-        
-                
+            })
+        }
 
-})
-        })})
+
+        )})
     })
 
     let searchEngine = (value)=>{
@@ -63,14 +62,14 @@ document.addEventListener("DOMContentLoaded",() =>{
         let voteBtn = document.createElement('button')
 
         
-        image.src = team.image
-        votes.textContent = `Votes: ${team.votes}`
-        voteBtn.textContent = 'like'
+        // // image.src = team.image
+        // votes.textContent = `Votes: ${team.votes}`
+        // voteBtn.textContent = 'like'
 
         
-        display.appendChild(votes)
-        display.appendChild(image)
-        display.appendChild(voteBtn)
+        // display.appendChild(votes)
+        // display.appendChild(image)
+        // display.appendChild(voteBtn)
         
 
         voteBtn.addEventListener('click', () =>{
@@ -92,34 +91,113 @@ document.addEventListener("DOMContentLoaded",() =>{
             }
         )})
 
-        fetch(`http://localhost:3000/squad/${value}`, {
-                method: 'PUT',
-                headers: {
-                'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(squad)
-            })
-                .then(response => response.json())
-                .then(data => {
-                // Log the updated data for debugging purposes
-                console.log(data);
-                searchEngine(value)
-                })
-                .catch(error => {
-                // Handle errors
-                console.error(error);
-                
-                })
+
+
+
+
+
+        
+let form = document.getElementById('forms')
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    let input = document.getElementById('formInput')
+    
+    console.log(input.value);
+        
+    let team = document.getElementById('teams')
+    team.remove()
+
+    let body = document.getElementById('body')
+        
+        
+        let main = document.createElement('div')
+        main.style = 'display: flex;height: 550px; width: 100%; background: none;'
+
+        let left = document.createElement('div')
+        let right = document.createElement('div')
+
+        left.style = 'display: block; margin-top: 2%; margin-left: 2%; width: 20%; height: 90%; background: none;'
+        right.style = 'display: flex; margin-top: 2%; margin-left: 5%; width: 85%; height: 30%; background: none;'
+
+        body.appendChild(main)
+        main.appendChild(left)
+        main.appendChild(right)
+
+        let left1 = document.createElement('div')
+        let left2 = document.createElement('div')
+
+        left1.style = 'color: white; margin-bottom: 4%; width: 100%; height:56%; border: 1px solid white;background: none; border-radius:14px;'
+        left2.style = 'color: white; width: 100%; height:40%; border: 1px solid white;background: none; border-radius:14px;'
+
+        left.appendChild(left1)
+        left.appendChild(left2)
+
+        let right1 = document.createElement('div')
+        let right2 = document.createElement('div')
+        let right3 = document.createElement('div')
+
+        right1.style = 'color: white; margin-right: 2%; width: 30%; height:100%; border: 1px solid white;background: none; border-radius:9px;'
+        right2.style = 'color: white; margin-right: 2%; width: 38%; height:100%; border: 1px solid white;background: none; border-radius:9px;'
+        right3.style = 'color: white; width: 30%; height:100%; border: 1px solid white;background: none; border-radius:9px;'
+
+        right.appendChild(right1)
+        right.appendChild(right2)
+        right.appendChild(right3)
+
+    
+        let teamNames = ['arsenal', 'chelsea', 'manchester', 'city', 'tottenham']
+        if(input.value.toLowerCase() == teamNames[0]){
+            body.style = 'background:url("https://www.bms.co.in/wp-content/uploads/2014/12/Arsenal-Football-Club2.jpg");background-size: cover; background-repeat: no-repeat;  display: block;'
             
-                .catch(error => {
-                // Handle errors
-                console.error(error);
+            left1.textContent = 'NAME:ARSENAL (ARS)'
+            left2.textContent = 'ALTERNATIVES:ARSENAL FOOTBALL CLUB, AFC,ARSENAL FC'
+    
+            right1.textContent = 'NICKNAME:Gunners/ Gooners'
+            right2.textContent = 'STADIUM:Emirates Stadium(London, England)'
+            right3.textContent = 'MANAGER:Mikel Arteta'
+        }
+        if(input.value.toLowerCase() == teamNames[1]){
+            body.style = 'background:url("https://c4.wallpaperflare.com/wallpaper/838/610/492/chelsea-fc-chelsea-football-club-logo-wallpaper-preview.jpg");background-size: cover; background-repeat: no-repeat;  display: block;'
             
-            })
-  
-    fetch("http://localhost:3000/squad/6")
-    .then (response => response.json())
-    .then (data => {
-        data.teams.backround
-    }
-    )
+            left1.textContent = 'NAME: Chelsea FC(CHE)'
+            left2.textContent = 'ALTERNATIVES:Chelsea'   
+    
+            right1.textContent = 'NICKNAME: The Blues, The Pensioners'
+            right2.textContent = 'STADIUM:Stamford Bridge (London, England)'
+            right3.textContent = 'MANAGER: Graham Potter'
+        }
+        if(input.value.toLowerCase() == teamNames[2]){
+            body.style = 'background:url("https://www.wallpaperflare.com/static/985/259/800/manchester-united-soccer-clubs-premier-league-typography-wallpaper.jpg");background-size: cover; background-repeat: no-repeat;  display: block;'
+            
+            left1.textContent = 'NAME:MANCHESTER UNITED (MNU)'
+            left2.textContent = 'ALTERNATIVES:Man United/ Man U / United'
+    
+            right1.textContent = 'NICKNAME:The Red Devils'
+            right2.textContent = 'STADIUM:Old Trafford (Manchester, UK)'
+            right3.textContent = 'MANAGER:Erik Ten Hag'
+        }
+        if(input.value.toLowerCase() == teamNames[3]){
+            body.style = 'background:url("https://e1.pxfuel.com/desktop-wallpaper/124/665/desktop-wallpaper-manchester-city-fc-manchester-city-2017.jpg'
+            
+            left1.textContent = 'NAME:MANCHESTER CITY(MNC)'
+            left2.textContent = 'ALTERNATIVES:MAN CITY'
+    
+            right1.textContent = 'NICKNAME:The Citizens, City, The Sky Blues'
+            right2.textContent = 'STADIUM:Etihad Stadium (Manchester, UK)'
+            right3.textContent = 'MANAGER:Pep Guardiola'
+        }
+        if(input.value.toLowerCase() == teamNames[4]){
+            body.style = 'background:url("https://w0.peakpx.com/wallpaper/341/185/HD-wallpaper-tottenham-xs-max-aia-fifa-kane-london-nike-premier-leauge-spurs-tottenham-hotspur-white-heart-line-xs-max-iphone.jpg");background-size: cover; background-repeat: no-repeat;  display: block;'
+            
+            left1.textContent = 'NAME: Tottenham Hotspur(TOT)'
+            left2.textContent = 'ALTERNATIVES:Tottenham'
+    
+            right1.textContent = 'NICKNAME:Spurs'
+            right2.textContent = 'STADIUM:Tottenham Hotspur Stadium(London, UK)'
+            right3.textContent = 'MANAGER:Cristian Stellini'
+        }
+    
+
+    form.reset()
+})
